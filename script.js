dd8155e6d275b45909c24a6dc93359d3fbc5223f
turstoncodes <53752666+turstoncodes@users.noreply.github.com>
2019-08-08 16:06:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// coded by TLouw 
-
-const projectName = 'technical-docs-page';
-localStorage.setItem('example_project', 'Technical Docs Page');
+// coded by TLouw
+// eslint-disable-next-line no-unused-vars
+const projectName = 'portfolio';
